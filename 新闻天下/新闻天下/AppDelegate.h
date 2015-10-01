cfdb33aaa8ed55cb174e51fd97c55b5779baabd8
唐枫 <tsz@qq.com>
2015-09-30 17:23:11 -0700

    第一次

---
+++ 新闻天下/新闻天下/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻天下
+//
+//  Created by mac on 15-9-30.
+//  Copyright (c) 2015年 tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新闻天下/新闻天下/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  新闻天下
+//
+//  Created by mac on 15-9-30.
+//  Copyright (c) 2015年 tsz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
